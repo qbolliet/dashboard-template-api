d2ce74923dfd9fa909ef62eeffb8c2e830361d09
Quentin Bolliet <bolliet.quentin@gmail.com>
2025-02-20 20:09:29 +0100

    (add) caching redis

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -3,13 +3,22 @@ const { redis } = require('../cache');
 
 // Création d'une fonction de cache
 const withCache = async (key, loader) => {
-  const cached = await redis.get(key);
-  if (cached) {
-    return JSON.parse(cached);
+  // Tentative de connexion au cache
+  try {
+      // Extraction du cache
+      const cached = await redis.get(key);
+      if (cached) {
+          return JSON.parse(cached);
+      }
+      // Sinon chargement des données
+      const result = await loader();
+      await redis.set(key, JSON.stringify(result), 'EX', 300);
+      return result;
+  } catch (error) {
+      // console.error('Cache operation failed:', error);
+      // Si le cache échoue, on  retombe sur le loader
+      return loader();
   }
-  const result = await loader();
-  await redis.set(key, JSON.stringify(result), 'EX', 300);
-  return result;
 };
 
 exports.withCache = withCache;
