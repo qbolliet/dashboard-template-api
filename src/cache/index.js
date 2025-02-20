a876a05c72188bc7b98702439498e3f93b31b2b4
Quentin Bolliet <bolliet.quentin@gmail.com>
2025-02-20 20:09:29 +0100

    (add) caching redis

--- src/cache/index.js
+++ src/cache/index.js
@@ -1,8 +1,12 @@
 // Importation des éléments du dossier
 const { createRedisClient } = require('./redis');
 
+// Initialisation d'un instance redis utilisée dans l'application
+const redis = createRedisClient();
+
 // Ré-exportation des fonctions d'intérêt
 module.exports = {
+    redis,
     createRedisClient
 };
   
