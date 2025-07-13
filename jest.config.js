dd4a7f3a2b84da755bb39a1dd1cc17ea8cca48d7
Quentin Bolliet <bolliet.quentin@gmail.com>
2025-07-13 20:29:14 +0200

    (sanitize) ES module handling

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ export default {
   // Utiliser le testEnvironment node pour les tests backend
   testEnvironment: 'node',
   
-  // Support des modules ES6
-  extensionsToTreatAsEsm: ['.js'],
+  // Support des modules ES6 (removed .js as it's inferred from package.json type: module)
+  preset: 'default',
   
   // Transformation des modules
   transform: {},
